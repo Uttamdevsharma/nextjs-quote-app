d6969e966db1fd9c49f292a6815a5cab1e077a99
Uttam <uttamrohit4545@gmail.com>
2025-07-17 23:42:10 +0600

    fast route page implemented

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <div>
+      <h1>Welcome to the Quote App</h1>
+      <p>Explore motivational and inspiring quotes!</p>
+    </div>
   );
 }
